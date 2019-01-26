1859bca46359f25df8376938ec72081e49bccec5
rasmusandre <rasmusandre@gmail.com>
2019-01-26 10:31:07 +0100

    Updated DB

--- fesi_ce/fesi_ce.py
+++ fesi_ce/fesi_ce.py
@@ -12,11 +12,11 @@ def main():
     max_cluster_size=4, size=[2,2,2],
     max_cluster_dia=[0.0, 10, 10, 7.5, 5.5], concentration=conc, cubic=True)
     #print(ceBulk.cluster_info_given_size(4))
-    #ceBulk.reconfigure_settings()
+    ceBulk.reconfigure_settings()
     #ceBulk.view_clusters()
     struct_generator = NewStructures(ceBulk, struct_per_gen=10)
 
-    #reconfigure(ceBulk)
+    reconfigure(ceBulk)
     evaluate(ceBulk)
     #insert_experimental_fesi_structure(struct_generator)
 
@@ -30,15 +30,50 @@ def reconfigure(ceBulk):
 def evaluate(ceBulk):
 
     from ase.clease import GAFit, Evaluate
-    ga = GAFit(setting=ceBulk, alpha=1E-8, mutation_prob=0.01, num_individuals="auto",
-    change_prob=0.2, fname="ga_fesi.csv", max_cluster_dia=6)
-    names = ga.run(min_change=0.001, gen_without_change=100)
+    #ga = GAFit(setting=ceBulk, alpha=1E-8, mutation_prob=0.01, num_individuals="auto",
+    #change_prob=0.2, fname="ga_fesi.csv", max_cluster_dia=6)
+    #names = ga.run(min_change=0.001, gen_without_change=100)
     with open("ga_fesi_cluster_names.txt", 'r') as infile:
         lines = infile.readlines()
     names = [x.strip() for x in lines]
-    compressive = BayesianCompressiveSensing(noise=0.1)
-    evaluator = Evaluate(ceBulk, fitting_scheme="l2", parallel=False, alpha=1E-8,
-    scoring_scheme="loocv_fast", cluster_names=names)
+    #compressive = BayesianCompressiveSensing(noise=0.1)
+    #evaluator = Evaluate(ceBulk, fitting_scheme="l2", parallel=False, alpha=1E-8,
+    #scoring_scheme="loocv_fast", cluster_names=names)
+    evaluator = Evaluate(ceBulk, fitting_scheme="l2", parallel=False, alpha=1E-1,
+    scoring_scheme="loocv_fast", max_cluster_dia=6, max_cluster_size=2)
+    #evaluator = Evaluate(ceBulk, fitting_scheme=compressive, parallel=False,
+    #scoring_scheme="loocv_fast")
+    #x = evaluator.cf_matrix
+    #k = x.T.dot(x)
+    #print(k.shape)
+    #print(np.linalg.matrix_rank(x.T.dot(x)))
+    evaluator.plot_fit(interactive=True)
+    eci_names = evaluator.get_cluster_name_eci(return_type="dict")
+
+    with open(eci_fname,'w') as outfile:
+        json.dump(eci_name, outfile, indent=2, separators=(",",":"))
+
+def convex_hull():
+    from ase.clease import ConvexHull
+    import matplotlib.pyplot as plt
+    ch = ConvexHull('FeSi_8atoms_12finished.db')
+    ch.plot()
+    plt.show()
+
+def evaluate_L2(ceBulk):
+
+    from ase.clease import GAFit, Evaluate, LinearRegression
+    #ga = GAFit(setting=ceBulk, alpha=1E-8, mutation_prob=0.01, num_individuals="auto",
+    #change_prob=0.2, fname="ga_fesi.csv", max_cluster_dia=6)
+    #names = ga.run(min_change=0.001, gen_without_change=100)
+    with open("ga_fesi_cluster_names.txt", 'r') as infile:
+        lines = infile.readlines()
+    names = [x.strip() for x in lines]
+    #compressive = BayesianCompressiveSensing(noise=0.1)
+    #evaluator = Evaluate(ceBulk, fitting_scheme="l2", parallel=False, alpha=1E-8,
+    #scoring_scheme="loocv_fast", cluster_names=names)
+    evaluator = Evaluate(ceBulk, fitting_scheme="l2", parallel=False, alpha=1E-6,
+    scoring_scheme="loocv_fast")
     #evaluator = Evaluate(ceBulk, fitting_scheme=compressive, parallel=False,
     #scoring_scheme="loocv_fast")
     #x = evaluator.cf_matrix
@@ -59,7 +94,7 @@ def insert_experimental_fesi_structure(struct_gen):
     init_structure = read('initial.xyz')
     final_structure = read('final.xyz')
 
-    calc = SinglePointCalculator(final_structure, energy=-135.839)
+    calc = SinglePointCalculator(final_structure, energy=-105.914)
     final_structure.set_calculator(calc)
     struct_gen.insert_structure(init_struct=init_structure, final_struct=final_structure, generate_template=True)
 
@@ -69,17 +104,16 @@ def create_xyz():
     from ase.db import connect
     from ase.io import write
 
-    db = connect('FeSi_8atoms.db')
+    db = connect('FeSi_16atoms.db')
 
-    initial = db.get_atoms(id=41)
-    final = db.get_atoms(id=42)
+    initial = db.get_atoms(id=15)
+    final = db.get_atoms(id=16)
 
     initial.write('initial.xyz')
     final.write('final.xyz')
 
 
 
-
-
-main()
 #create_xyz()
+#main()
+convex_hull()
